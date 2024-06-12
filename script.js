7c4a95e6a646bcd4d623e6ce28682394ffa7dbb0
ariannaal <ariannaloreti25@gmail.com>
2024-06-12 17:44:43 +0200

    adds li

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ const fetchBooks = () => {
     // utilizziamo il dato come di consueto, dentro books.data c'è un semplice array che
         books.forEach(book => {
             const col = document.createElement("div")
-            col.classList.add("col-lg-3", "col-md-4")
+            col.classList.add("col-lg-3", "col-md-4", "col-sm-6")
             const card = document.createElement("div")
             card.classList.add("card")
 
@@ -50,20 +50,30 @@ const fetchBooks = () => {
 
         //creo l'elemento h5 per il titolo della card
         const title = document.createElement("h5");
-        title.classList.add("card-title");
+        title.classList.add("card-title")
         title.textContent = book.title;
 
         // creo l'elemento per il prezzo
         const price = document.createElement("div");
         price.classList.add("card-price");
         price.textContent = book.price + "$";
-            
+        
+        //creo l'elemento per il bottone
         const buttonScarta = document.createElement("button");
-        buttonScarta.classList.add("btn", "btn-danger", "m-2");
+        buttonScarta.classList.add("btn", "btn-danger");
         buttonScarta.textContent = "Scarta";
         buttonScarta.addEventListener('click', () => {
                     card.remove();
-                });
+        });
+            
+     // creo il bottone per il carrello
+            const cartButton = document.createElement("button");
+            cartButton.classList.add("btn", "btn-primary", "mt-1");
+            cartButton.textContent = "Compra ora";
+            cartButton.addEventListener("click", () => {
+            addToCart(book);
+            });
+            
             
         col.appendChild(card);
         row.appendChild(col);
@@ -72,7 +82,7 @@ const fetchBooks = () => {
         cardBody.appendChild(title);
         cardBody.appendChild(price);
         cardBody.appendChild(buttonScarta);
-          
+         cardBody.appendChild(cartButton);  
 
     })
 
@@ -80,9 +90,26 @@ const fetchBooks = () => {
 })
 };
 
+//dichiara una funzione chiamata addToCart che prende un parametro book, che rappresenta il libro da aggiungere al carrello.
+const addToCart = (book) => {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+    cart.push(book);
+    localStorage.setItem("cart", JSON.stringify(cart)); // aggiorna l'array del carrello nello storage locale
+    showCart();
+};
 
+//questa funzione recupera il contenuto del carrello dallo storage locale, lo mostra nell'interfaccia utente sotto forma di ul, e per ogni elemento nel carrello, mostra il titolo dell'elemento come elemento della lista
+const showCart = () => {
+    const cartItems = JSON.parse(localStorage.getItem("cart"));
+    const cartList = document.getElementById("carrello");
+    cartList.innerHTML = "";
+    cartItems.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = item.title;
+        li.classList.add("list-group-item");
+        cartList.appendChild(li);
 
-
-
+    });
+};
 
 
